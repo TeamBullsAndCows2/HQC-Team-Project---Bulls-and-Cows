7887e3758902856e8792c563e25f49a426b61b8f
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-07-15 10:09:00 +0300

    namespaces change, GameScore class house-keeping

--- gameScore.cs
+++ gameScore.cs
@@ -1,42 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Runtime.Serialization;
-
-namespace bikove
+﻿namespace BullsAndCows
 {
-    public class gameScore : IComparable
+    using System;
+
+    public class GameScore : IComparable
     {
-        public gameScore(string ime, int guesses)
+        public GameScore(string name, int guesses)
         {
-            this.Name = ime;
+            this.Name = name;
             this.Guesses = guesses;
         }
 
-        public string Name
-        {
-            get;
-            private set;
-        }
-        public int Guesses
-        {
-            get;
-            private set;
-        }
+        public string Name { get; private set; }
+
+        public int Guesses { get; private set; }
+
         public override bool Equals(object obj)
         {
-            gameScore objectToCompare = obj as gameScore;
+            GameScore objectToCompare = obj as GameScore;
+
             if (objectToCompare == null)
             {
                 return false;
             }
             else
             {
-
-
-
-                return this.Guesses.Equals(objectToCompare) && this.Name.Equals(objectToCompare);
+                return this.Guesses.Equals(objectToCompare.Guesses) && this.Name.Equals(objectToCompare.Name);
             }
         }
 
@@ -50,19 +38,19 @@ namespace bikove
             return string.Format("{0} --> {1} {2}", this.Name, this.Guesses, this.Guesses == 1 ? "guess" : "guesses");
         }
 
+        // TODO: Curently not used!
         public int CompareTo(object obj)
         {
-            gameScore objectToCompare = obj as gameScore;
+            GameScore objectToCompare = obj as GameScore;
+
             if (objectToCompare == null)
             {
                 return -1;
             }
+
             if (this.Guesses.CompareTo(objectToCompare.Guesses) == 0)
             {
-                
-				
-				
-				return this.Name.CompareTo(objectToCompare.Name);
+                return this.Name.CompareTo(objectToCompare.Name);
             }
             else
             {
@@ -70,24 +58,27 @@ namespace bikove
             }
         }
 
-        public string Serialize()
+        public string FormatGameScore()
         {
             return string.Format("{0}_:::_{1}", this.Name, this.Guesses);
-
-
-
         }
-        public static gameScore Deserialize(string data)
+
+        public static GameScore GetGameScore(string data)
         {
             string[] dataAsStringArray = data.Split(new string[] { "_:::_" }, StringSplitOptions.None);
-            if (dataAsStringArray.Length != 2) return null;
+
+            if (dataAsStringArray.Length != 2)
+            {
+                throw new ArgumentException("Data is not in the correct format!");
+            }
 
             string name = dataAsStringArray[0];
 
             int guesses = 0;
+
             int.TryParse(dataAsStringArray[1], out guesses);
 
-            return new gameScore(name, guesses);
+            return new GameScore(name, guesses);
         }
     }
 }
