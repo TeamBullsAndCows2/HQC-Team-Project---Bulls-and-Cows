32ad58b615815bf2f40423b68aa44c3bce9d7639
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-07-17 17:15:34 +0300

    Added Test Solution For all test classes + SingleTon test

---
+++ BullsAndCows.Test/RandomGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿namespace BullsAndCows.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BullsAndCows.Tools;
+
+    [TestClass]
+    public class RandomGeneratorTest
+    {
+        [TestMethod]
+        public void CheckForOnlyOneInstance()
+        {
+            var fistInstance = RandomGenerator.Instance;
+            var secondInstance = RandomGenerator.Instance;
+
+            Assert.AreEqual(fistInstance, secondInstance);
+        }
+    }
+}
