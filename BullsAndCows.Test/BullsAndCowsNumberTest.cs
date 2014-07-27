e60bebd9e1bf98a4d17111d0d9e55bb8175d1f51
SVladkov <s.v.vladkov@gmail.com>
2014-07-27 21:20:51 +0300

    Added CheatTest class

--- BullsAndCows.Test/BullsAndCowsNumberTest.cs
+++ BullsAndCows.Test/BullsAndCowsNumberTest.cs
@@ -15,5 +15,23 @@
             Assert.AreEqual(number.TryToGuess("1234").Bulls, 4);
             Assert.AreEqual(number.TryToGuess("1234").Cows, 0);
         }
+
+        [TestMethod]
+        public void TestGuessingOneCowAndOneBull()
+        {
+            BullsAndCowsNumber number = new BullsAndCowsNumber(new int[] { 1, 2, 3, 4 });
+
+            Assert.AreEqual(number.TryToGuess("1113").Bulls, 1);
+            Assert.AreEqual(number.TryToGuess("1113").Cows, 1);
+        }
+
+        [TestMethod]
+        public void TestGuessingOneCowAndOneBullSameDigit()
+        {
+            BullsAndCowsNumber number = new BullsAndCowsNumber(new int[] { 1, 1, 0, 0 });
+
+            Assert.AreEqual(number.TryToGuess("1221").Bulls, 1);
+            Assert.AreEqual(number.TryToGuess("1221").Cows, 1);
+        }
     }
 }
---
+++ BullsAndCows.Test/CheatTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Common.Test
+{
+    [TestClass]
+    public class CheatTest
+    {
+        [TestMethod]
+        public void TestGettingFourCheats()
+        {
+            int[] number = new int[] {1, 2, 3, 4};
+            Cheat cheat = new Cheat(number);
+            cheat.GetCheat();
+            cheat.GetCheat();
+            cheat.GetCheat();
+
+            Assert.AreEqual("1234", cheat.GetCheat());
+        }
+
+        [TestMethod]
+        public void TestFailingGettingFourCheats()
+        {
+            int[] number = new int[] { 1, 2, 3, 4 };
+            Cheat cheat = new Cheat(number);
+            cheat.GetCheat();
+            cheat.GetCheat();
+            cheat.GetCheat();
+
+            Assert.AreNotEqual("1233", cheat.GetCheat());
+        }
+    }
+}
