292b511f355678dc066a19ed1e56d6a202033117
SVladkov <s.v.vladkov@gmail.com>
2014-07-27 21:38:00 +0300

    Added CheckerTest class

---
+++ BullsAndCows.Test/CheckerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Common.Test
+{
+    [TestClass]
+    public class CheckerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestArgumentException()
+        {
+            int guesses = 0;
+            Checker.TryToGuess("123", ref guesses, new int[] {1, 2, 3, 4});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestArgumentExceptionArgumentTooLong()
+        {
+            int guesses = 0;
+            Checker.TryToGuess("12333", ref guesses, new int[] { 1, 2, 3, 4 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestArgumentExceptionArgumentNotNumber()
+        {
+            int guesses = 0;
+            Checker.TryToGuess("asdfg", ref guesses, new int[] { 1, 2, 3, 4 });
+        }
+
+        [TestMethod]
+        public void TestFullyGuessing()
+        {
+            int guesses = 0;
+            Result result = Checker.TryToGuess("1234", ref guesses, new int[] { 1, 2, 3, 4 });
+
+            Assert.AreEqual(guesses, 1);
+            Assert.AreEqual(result.Bulls, 4);
+            Assert.AreEqual(result.Cows, 0);
+        }
+    }
+}
