cce0a40bdbd3ef4f0a956965ba68abcf4d7e77c4
IvailoPapazov <ihellfiend@gmail.com>
2014-07-30 13:12:44 +0300

    Modified random generator.
    Array generator now generates distinct values array.
    Added test.
    Added documentation.

--- Tools/RandomGenerator.cs
+++ Tools/RandomGenerator.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using BullsAndCows.Common.Interfaces;
+    using System.Linq;
 
     /// <summary>
     /// Singleton Random generator class.
@@ -10,12 +11,16 @@
     {
         private static readonly RandomGenerator instance = new RandomGenerator();
         private readonly Random randomGen;
+        private const int UpperBound = 9;
 
         private RandomGenerator()
         {
             this.randomGen = new Random();
         }
 
+        /// <summary>
+        /// Gets the only instance of RandomGenerator class.
+        /// </summary>
         public static RandomGenerator Instance 
         {
             get
@@ -29,22 +34,55 @@
             }
         }
 
+        /// <summary>
+        /// Returns random value within a range.
+        /// </summary>
+        /// <param name="minValue">Lower bound of generated numbers</param>
+        /// <param name="maxValue">Higher bound of generated numbers</param>
+        /// <returns>Generated random number within provided range.</returns>
         public int GetValue(int minValue, int maxValue)
         {
             return this.randomGen.Next(minValue, maxValue + 1);
         }
 
+        /// <summary>
+        /// Generate array with four distinct digits
+        /// </summary>
+        /// <returns>Generated array</returns>
         public int[] GenerateRandomFourDigitArray()
         {
             int[] number = new int[4];
-            number[0] = this.GetValue(1, 9);
+            number[0] = this.GetValue(1, UpperBound);
 
             for (int i = 1; i < number.Length; i++)
             {
-                number[i] = this.GetValue(0, 9);
+                number[i] = GetDistinctDigit(number, UpperBound);
             }
 
             return number;
+        }
+ 
+        private int GetDistinctDigit(int[] number, int upperBound)
+        {
+            if (upperBound <= number.Length)
+            {
+                throw new ArgumentOutOfRangeException("Upper bound must be greater or equal than number array!");
+            }
+
+            int distinctDigit = 0;
+
+            bool isDistinctDigit = false;
+            while (!isDistinctDigit)
+            {
+                int generatedDigit = this.GetValue(0, upperBound);
+                if (!number.Contains(generatedDigit))
+                {
+                    distinctDigit = generatedDigit;
+                    isDistinctDigit = true;
+                }
+            }
+
+            return distinctDigit;
         }
     }
 }
